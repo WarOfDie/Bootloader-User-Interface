b19849c0f2a5081c7b2d69983808ae7798b206e5
WarOfDie <94315325+WarOfDie@users.noreply.github.com>
2024-01-13 22:08:13 +0300

    Add project files.

---
+++ myTerminal6/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace myTerminal6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.btnKapa = new System.Windows.Forms.Button();
+            this.textBound = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textGelenveri = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnBoot = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(12, 328);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(279, 23);
+            this.btnGonder.TabIndex = 0;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Com Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAc
+            // 
+            this.btnAc.Location = new System.Drawing.Point(198, 22);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(75, 23);
+            this.btnAc.TabIndex = 2;
+            this.btnAc.Text = "Aç";
+            this.btnAc.UseVisualStyleBackColor = true;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // btnKapa
+            // 
+            this.btnKapa.Location = new System.Drawing.Point(198, 49);
+            this.btnKapa.Name = "btnKapa";
+            this.btnKapa.Size = new System.Drawing.Size(75, 23);
+            this.btnKapa.TabIndex = 3;
+            this.btnKapa.Text = "Kapat";
+            this.btnKapa.UseVisualStyleBackColor = true;
+            this.btnKapa.Click += new System.EventHandler(this.btnKapa_Click);
+            // 
+            // textBound
+            // 
+            this.textBound.Location = new System.Drawing.Point(79, 51);
+            this.textBound.Name = "textBound";
+            this.textBound.Size = new System.Drawing.Size(100, 20);
+            this.textBound.TabIndex = 5;
+            this.textBound.Text = "115200";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Boudrate";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textGelenveri
+            // 
+            this.textGelenveri.BackColor = System.Drawing.SystemColors.Window;
+            this.textGelenveri.Location = new System.Drawing.Point(12, 103);
+            this.textGelenveri.Multiline = true;
+            this.textGelenveri.Name = "textGelenveri";
+            this.textGelenveri.Size = new System.Drawing.Size(279, 190);
+            this.textGelenveri.TabIndex = 7;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // btnBoot
+            // 
+            this.btnBoot.Location = new System.Drawing.Point(12, 299);
+            this.btnBoot.Name = "btnBoot";
+            this.btnBoot.Size = new System.Drawing.Size(279, 23);
+            this.btnBoot.TabIndex = 8;
+            this.btnBoot.Text = "Bootloader Giriş";
+            this.btnBoot.UseVisualStyleBackColor = true;
+            this.btnBoot.Click += new System.EventHandler(this.btnBoot_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnAc);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnKapa);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBound);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(279, 85);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Seri Port Ayarları";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtVersion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtVersion.Location = new System.Drawing.Point(65, 275);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(36, 13);
+            this.txtVersion.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(15, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Versiyon:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 367);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVersion);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBoot);
+            this.Controls.Add(this.textGelenveri);
+            this.Controls.Add(this.btnGonder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UART Bootloader Arayüzü";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button btnKapa;
+        private System.Windows.Forms.TextBox textBound;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textGelenveri;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnBoot;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ myTerminal6/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Amazon.S3.Model;
+using System;
+using System.IO;
+using System.IO.Ports;
+using System.Web;
+using System.Windows.Forms;
+using XModemProtocol;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace myTerminal6
+{
+
+    public partial class Form1 : Form
+    {
+
+        string gelenveri = string.Empty;
+        string[] parcalar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort1.IsOpen)
+            {
+                string uzanti;
+
+                //XModemCommunicator başlat.
+                var xmodem = new XModemCommunicator();
+
+
+                // Port ekle
+                xmodem.Port = serialPort1;
+
+                // Datayı al.
+                OpenFileDialog dialog = new OpenFileDialog();
+
+                dialog.Filter = "Binary Files (*.bin) |*.bin|Hex Files (*.hex) |*.hex"; ;
+
+                openFileDialog.InitialDirectory = @"C:\";
+                
+                
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                      uzanti = Path.GetExtension(dialog.FileName);
+
+                    if (uzanti.Equals(".hex", StringComparison.OrdinalIgnoreCase))
+                    {
+                        string filepath = string.Empty;
+                        string outputFilepath = string.Empty;
+                        filepath = " "+dialog.FileName+" ";
+                        outputFilepath = Environment.GetFolderPath(
+                        Environment.SpecialFolder.Desktop);
+                        outputFilepath += "\\bin.bin";
+                        Process p = new Process();
+                        string projedizini = Environment.CurrentDirectory;
+                        p.StartInfo.FileName =projedizini + "\\MinGW\\bin\\objcopy.exe";
+                        p.StartInfo.Arguments = "--input-target=ihex --output-target=binary" + filepath + outputFilepath;
+                        p.Start();
+                        dialog.FileName = outputFilepath;
+                        Thread.Sleep(1000);
+                    }
+                    xmodem.Data = File.ReadAllBytes(dialog.FileName);
+                }
+                
+                // Datayı gönder.
+                xmodem.Mode = XModemMode.CRC;
+                xmodem.Mode.Equals(xmodem.Mode);
+                xmodem.Send();
+
+                gelenveri = string.Empty;
+                textGelenveri.Text = " ";
+                txtVersion.Text = " ";
+                // Datanın gönderildiğini kontrol et.
+                if (xmodem.State != XModemStates.Idle)
+                {
+                    xmodem.CancelOperation();
+
+                }
+                string dosyaAdi = Path.GetFileName(dialog.FileName);
+                if (dosyaAdi.Equals("bin.bin", StringComparison.OrdinalIgnoreCase))
+                {
+                    File.Delete(dialog.FileName);
+                }
+                    
+                
+                
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            // Seri portu aç.
+            if (!serialPort1.IsOpen && comboBox1.Text != string.Empty)
+            {
+                serialPort1.BaudRate = Convert.ToInt32(textBound.Text);
+                
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();
+            }
+            
+        }
+        
+        private void btnKapa_Click(object sender, EventArgs e)
+        {
+            // Seri portu kapat.
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // C karakteri haricinde mikrodenetleyiciden gelen verileri textbox'a yazdır.
+            if (!gelenveri.Contains("C"))
+            {
+                gelenveri = serialPort1.ReadExisting();
+                parcalar = gelenveri.Split('&');
+                this.Invoke(new EventHandler(DisplayText));
+
+                
+               
+            }
+           
+
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            // Event handler
+             textGelenveri.Text = textGelenveri.Text + parcalar[0];
+            if (1 < parcalar.Length)
+            {
+                txtVersion.Text = parcalar[1];
+            }
+        }
+
+        private void btnBoot_Click(object sender, EventArgs e)
+        {
+            // Bootloader moduna girmek için mikrodenetleyiciye anahtar gönder.
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Write("1");
+                textGelenveri.Text = " ";
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
